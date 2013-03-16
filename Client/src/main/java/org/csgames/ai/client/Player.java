a69b052c521c910d09e548c67574e392b93c9050
Antoine Grondin <antoinegrondin@gmail.com>
2013-03-16 10:45:21 -0400

    Fix bugs in concurrent mod of bomblist. Debug output.

--- Client/src/main/java/org/csgames/ai/client/Player.java
+++ Client/src/main/java/org/csgames/ai/client/Player.java
@@ -6,77 +6,92 @@ import java.util.List;
 import org.csgames.ai.client.Util.Point2D;
 
 public class Player {
-	
-	private int mBombLeft = 0;
+
+	private int mBombLeft = Values.DEFAULT_BOMB_COUNT;
 	private int mPowUpBomb = 0;
 	private int mPowUpRange = 0;
 	private boolean mHasPowUpDet = false;
 	private boolean mHasPowUpKick = false;
 	private String mPlayerNumber = "";
 	private Point2D mLocation;
-	
+
 	private List<PlayerBomb> mBombList = new ArrayList<PlayerBomb>();
-	
+
 	public Player(String number){
 		mPlayerNumber = number;
 	}
-	
+
 	// Player current state
-	
+
 	public void update(long time){
 		if(!hasDetonationPU()){
-			for(PlayerBomb bomb : mBombList){
+			
+			PlayerBomb[] bombs = mBombList.toArray(new PlayerBomb[mBombList.size()]);
+			
+			for(PlayerBomb bomb : bombs){
+				System.out.println(bomb);
 				if(bomb.timeBeforeExplode(time) <= 0) {
-					explode(bomb);
+					mBombList.remove(bomb);
+					mBombLeft++;
 				}
-			}
+			}	
+			
 		}
 	}
-	
+
 	// Get/set
-	
+
 	public int getBombLeft(){ return mBombLeft + mPowUpBomb; }
 	public List<PlayerBomb> getBombList(){ return mBombList; }
-	
+
 	public String getPlayerNumber(){ return mPlayerNumber; }
-	
+
 	public boolean addBomb(PlayerBomb b){
 		if(getBombLeft() <= 0) return false;
 		mBombLeft--;
 		return mBombList.add(b);
 	}
-	
-	public void explode(PlayerBomb b){
-		mBombLeft++;
-		mBombList.remove(b);
-	}
-	
+
 	public void setLocation(int x, int y) {
 		mLocation = new Point2D(x,y);
 	}
-	
+
 	public Point2D getLocation(){ return mLocation; }
-	
+	public int getTotalRange(){ 
+		return getRangePU() + Values.DEFAULT_BOMB_RANGE;
+	}
+
 	// Powerups
 	public int getBombPU(){ return mPowUpBomb; }
 	public void addBombPU(){ mPowUpBomb++; }
-	
+
 	public int getRangePU(){ return mPowUpRange; }
 	public void addRangePU(){ mPowUpRange++; }
-	
+
 	public boolean hasDetonationPU(){ return mHasPowUpDet; }
 	public void setDetonationPU(){ mHasPowUpDet = true; }
-	
+
 	public boolean hasKickPU(){ return mHasPowUpKick; }
 	public void setKickPU(){ mHasPowUpKick = true; }
-	
-	
+
+	public String toString(){
+		return String.format("Player %s@%s, bomb_left:%d, bomb_range:%d, bomb_pu:%d, range_pu:%d",
+				mPlayerNumber, 
+				mLocation,
+				getBombLeft(),
+				getTotalRange(),
+				getBombPU(),
+				getRangePU()
+				);
+	}
+
 	// Helper classes
 	public static class Values{
 		public final static int DEFAULT_BOMB_RANGE = 2;
+		public final static int DEFAULT_BOMB_COUNT = 3;
 		public final static long DEFAULT_BOMB_DET_TIME = 1000;
 	}
-	
+
 	public class PlayerBomb{
 		public final Point2D location;
 		public final long createdAt;
@@ -89,5 +104,10 @@ public class Player {
 			return Values.DEFAULT_BOMB_DET_TIME - (now - createdAt);
 		}
 		public int getRange(){ return Values.DEFAULT_BOMB_RANGE + mPowUpRange; }
+		public String getOwner(){ return mPlayerNumber; }
+		public String toString(){
+			long now = System.currentTimeMillis();
+			return String.format("bomb@%s from p%s ttl:%d", location, getOwner(), timeBeforeExplode(now));
+		}
 	}
 }
